7e43939bb4cd3c90812d453b0f56e588c0e5bdc9
Mehdi Acho <mehdiacho@gmail.com>
2024-09-06 10:39:31 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Replace with your actual repository name
   plugins: [react()],
 })
